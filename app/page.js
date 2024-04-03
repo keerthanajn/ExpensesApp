78d6c1c7d9d53eb73f0a6f126ec1477fab5d8ac8
David Ghorbani-Nia <d.ghorbani-nia@se22.qmul.ac.uk>
2024-04-03 12:52:14 +0100

    updated

--- app/page.js
+++ app/page.js
@@ -1,13 +1,16 @@
 import Image from "next/image";
 import './global.css'
+import './page.css'
 
 export default function Home() {
   return (
-    <>
+    <div id="main_body">
       <div>
-        <h1>Expenses App</h1>
-        <a href="login"><button type="submit">Sign in</button></a>
+        <h1>FDM Expenses App</h1>
+        <div id="login_section">
+          <a href="login"><button id="sign_button"><span>Lets get started</span></button></a>
+        </div>
       </div>
-    </>
+    </div>
   )
 }
