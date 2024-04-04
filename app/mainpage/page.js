7aab10abe5d903e2993dd2f0b6109bc8649ab961
David Ghorbani-Nia <d.ghorbani-nia@se22.qmul.ac.uk>
2024-04-04 19:27:19 +0100

    added new sidebar

--- app/mainpage/page.js
+++ app/mainpage/page.js
@@ -68,11 +68,12 @@ export default function ButtonPage() {
     <>
       <header> <p><a href="mainpage" class="header_item">FDM Expenses App</a></p>   <p href="login" class="header_item"><a href="#home">Sign out</a></p> </header>
       <div id="sidebar">
-          <a href="view" class="sidebar_item">View Claims</a>
-          <a href="submit" class="sidebar_item">Submit a Claim</a>
-          <a href="reviews" class="sidebar_item">Review a Claim</a>
-          <a href="register" class="sidebar_item">Register an account</a>
-          <a href="profile" class="sidebar_item">Profile</a>
+        <a href="mainpage" class="sidebar_item">Dashboard</a>
+        <a href="view" class="sidebar_item">View Claims</a>
+        <a href="submit" class="sidebar_item">Submit a Claim</a>
+        <a href="reviews" class="sidebar_item">Review a Claim</a>
+        <a href="register" class="sidebar_item">Register an account</a>
+        <a href="profile" class="sidebar_item">Profile</a>
       </div>
       <div id="contentbox">
             <h1 id="top_text">Welcome back *NAME*</h1>
