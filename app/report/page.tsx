635116447a7c03fdf21e70e2ec20881008e9fd3f
nikipala <47778944+nikipala@users.noreply.github.com>
2024-04-05 13:29:38 +0100

    Update page.tsx - changed layout to be same as register form

--- app/report/page.tsx
+++ app/report/page.tsx
@@ -1,32 +1,41 @@
 import React from "react";
-import "./review.css";
+import "../register/register.css";
 
 export default function Reviews() {
   return (
     <>
       <div id="contentbox">
-        <form>
-          <legend>Review issue</legend>
-          <input
-            id="user_id"
-            className="inputbox"
-            placeholder="User Id"
-          ></input>
-          <input
-            id="subject_textbox"
-            className="inputbox"
-            placeholder="Issue"
-            required
-          ></input>
-          <textarea
-            id="description_textbox"
-            className="inputbox"
-            placeholder="Description"
-            required
-          ></textarea>
-          <button id="clear_button">Clear</button>
-          <button id="submit_button">Submit</button>
-        </form>
+        <h1>Report Issue</h1>
+        <div className="register-box">
+          <form id="form" name="report-form" method="post" action="">
+            <fieldset>
+              <legend>Report an Issue</legend>
+              <br></br>
+
+              <section>
+                <label htmlFor="userNum">User Id:</label>
+                  <input
+                    type="text"
+                    id="userId"
+                    name="userId"
+                    maxLength={8}
+                />
+
+                <label htmlFor="iName">Issue Title:</label>
+                <input type="text" id="iname" name="iname" />
+
+                <label htmlFor="iDescription">Description:</label>
+                <input type="text" id="description" name="description" />
+
+              </section>
+
+              <div className="form-actions">
+                <button type="submit">Confirm</button>
+                <button type="submit">Back</button>
+              </div>
+            </fieldset>
+          </form>
+        </div>
       </div>
     </>
   );
