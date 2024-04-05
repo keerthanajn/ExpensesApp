56e8d90de14715bef740eef7c390c62266075dbc
Maxwell <120192706+VLazorkite@users.noreply.github.com>
2024-04-05 15:46:59 +0100

    Revert to "Some form changes"
    This reverts commit 69972bb151ce7396ff548cd6b360512262c7c949.

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -6,15 +6,39 @@ import Link from "next/link";
 export default function Register() {
   return (
     <>
-      <header> <p><a href="mainpage" className="header_item">FDM Expenses App</a></p>   <p className="header_item"><a href="login">Sign out</a></p> </header>
+      <header>
+        {" "}
+        <p>
+          <a href="mainpage" className="header_item">
+            FDM Expenses App
+          </a>
+        </p>{" "}
+        <p className="header_item">
+          <a href="login">Sign out</a>
+        </p>{" "}
+      </header>
       <div id="sidebar">
-        <a href="mainpage" className="sidebar_item">Dashboard</a>
-        <a href="view" className="sidebar_item">View Claims</a>
-        <a href="submit" className="sidebar_item">Submit a Claim</a>
-        <a href="report" className="sidebar_item">Report an Issue</a>
-        <a href="register" className="sidebar_item">Register an Account</a>
-        <a href="review" className="sidebar_item">Review a Claim</a>
-        <a href="profile" className="sidebar_item">Profile</a>
+        <a href="mainpage" className="sidebar_item">
+          Dashboard
+        </a>
+        <a href="view" className="sidebar_item">
+          View Claims
+        </a>
+        <a href="submit" className="sidebar_item">
+          Submit a Claim
+        </a>
+        <a href="report" className="sidebar_item">
+          Report an Issue
+        </a>
+        <a href="register" className="sidebar_item">
+          Register an Account
+        </a>
+        <a href="review" className="sidebar_item">
+          Review a Claim
+        </a>
+        <a href="profile" className="sidebar_item">
+          Profile
+        </a>
       </div>
       <div id="contentbox">
         <h1>Register</h1>
@@ -25,14 +49,11 @@ export default function Register() {
               <br></br>
 
               <section>
-                <label htmlFor="fName">First Name:</label>
+                <label htmlFor="name">Full Name:</label>
                 <input type="text" id="name" name="name" />
 
-                <label htmlFor="lName">Last Name:</label>
+                <label htmlFor="username">Username:</label>
                 <input type="text" id="username" name="username" />
-
-                <label htmlFor="email">Email:</label>
-                <input type="email" id="email" name="email" />
 
                 <label htmlFor="password">Password:</label>
                 <input
@@ -40,6 +61,17 @@ export default function Register() {
                   id="password"
                   name="password"
                   minLength={8}
+                />
+
+                <label htmlFor="email">Email:</label>
+                <input type="email" id="email" name="email" />
+
+                <label htmlFor="bio">Contact Number:</label>
+                <input
+                  type="number"
+                  id="contact"
+                  name="contact"
+                  minLength={12}
                 />
 
                 <label htmlFor="title">Title:</label>
