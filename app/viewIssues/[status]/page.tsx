fb7a5878bed653d909ef9e96ee288f11e696035a
nikipala <47778944+nikipala@users.noreply.github.com>
2024-04-06 19:47:37 +0100

    Create page.tsx - Issue Status

---
+++ app/viewIssues/[status]/page.tsx
@@ -0,0 +1,26 @@
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { findUser } from "../../DB/find";
+import { ticketfilter } from "../../DB/methods";
+import "../view/view.css";
+import _ from "lodash";
+import ListItem from "../../components/listItem";
+type Params = {
+  status: "unresolved" | "resolved" ;
+};
+
+export default async function RequestIssues({
+  params: { status },
+}: {
+  params: Params;
+}) {
+  const { getUser } = getKindeServerSession();
+  const user = await getUser();
+
+  if (!user || !user.email) {
+    return <h1>No user</h1>;
+  }
+
+  const currentUser = await findUser(user.email);
+  const filteredTickets = ticketfilter(currentUser);
+  return <ListItem tickets={filteredTickets} status={status} />;
+}
